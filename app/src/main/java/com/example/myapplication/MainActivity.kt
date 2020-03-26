cc1847e0acc7f0b379d6603c4d69a8337b90d6fe
SyrymPP <syrymalisher@gmail.com>
2020-03-26 17:48:16 +0600

    to check branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity(), PostAdapter.RecyclerViewItemClick {
 
     lateinit var recyclerView: RecyclerView
 
+//    to check branch
+
     private var postAdapter: PostAdapter? = null
     lateinit var swipeRefreshLayout: SwipeRefreshLayout
     lateinit var  bottomNavigationView: BottomNavigationView
